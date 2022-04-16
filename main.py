0551a575099e3116c25e5ab81133a286a4581b9c
Cristian Cázares <cm_cjavier@hotmail.com>
2022-04-16 19:45:45 +0000

    [ENHANCEMENT] Secret comimt

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 
 def main():
     file = open("file.txt", "a")
-    file.write("a")
+    file.write("a\n")
     file.close()
+    os.system("git add .")
+    os.system("git commit -m '[ENHANCEMENT] Secret comimt'")
+    os.system("git push origin main")
 
 
 main()
