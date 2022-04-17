e0fa1f0df2be8f168fa8a3267535fe6bd4ea3a35
CristianCazares <cm_cjavier@hotmail.com>
2022-04-17 19:08:18 +0000

    [ENHANCEMENT] Secret commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 
-os.syestem("")
 os.system("clear")
 os.system("git add .")
-os.system("git commig -m '[ENHANCEMENT] Secret commit'")
+os.system("git commit -m '[ENHANCEMENT] Secret commit'")
