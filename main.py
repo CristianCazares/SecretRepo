7efa4f100ea1fa58376ef4562fc681455e9d9aff
CristianCazares <cm_cjavier@hotmail.com>
2022-04-19 04:31:16 +0000

    [ENHANCEMENT] Secret commit

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import os
+import random
+import datetime
 
-os.system("clear")
-os.system("git add .")
-os.system("git commit -m '[ENHANCEMENT] Secret commit'")
-os.system("git push origin main")
+
+date = datetime.datetime.now()
+print(date.year)
+print(date.month)
+print(date.day)
+print(date.hour)
+print(date.minute)
+
+def commit():
+    os.system("clear")
+    os.system("git add .")
+    os.system("git commit -m '[ENHANCEMENT] Secret commit'")
+    os.system("git push origin main")
+def gitConfig():
+    os.system('git config --global user.email "cm_cjavier@hotmail.com"')
+    os.system('git config --global user.name "CristianCazares"')
+
+gitConfig()
+commit()
